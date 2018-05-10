acffceb24ac02d68e983ec0539751c19dbff940f
Yoshito Okada <okada@rm.is.tohoku.ac.jp>
2018-05-11 07:30:40 +0900

    support image_transport param

--- include/moving_object_detector/moving_object_detector.hpp
+++ include/moving_object_detector/moving_object_detector.hpp
@@ -3,6 +3,7 @@
 
 #include <image_transport/image_transport.h>
 #include <image_transport/subscriber.h>
+#include <image_transport/transport_hints.h>
 #include <nodelet/nodelet.h>
 #include <object_detection_msgs/Objects.h>
 #include <ros/node_handle.h>
@@ -24,13 +25,19 @@ private:
     // load params
     republish_image_ = pnh.param("republish_image", false);
 
-    // start detection
+    // setup result publishers
     image_transport::ImageTransport it(nh);
     if (republish_image_) {
       image_publisher_ = it.advertise("image_out", 1, true);
     }
     object_publisher_ = nh.advertise< object_detection_msgs::Objects >("objects_out", 1, true);
-    image_subscriber_ = it.subscribe("image_raw", 1, &MovingObjectDetector::detect, this);
+
+    // start detection
+    const image_transport::TransportHints default_hints;
+    image_subscriber_ =
+        it.subscribe("image_raw", 1, &MovingObjectDetector::detect, this,
+                     image_transport::TransportHints(default_hints.getTransport(),
+                                                     default_hints.getRosHints(), pnh));
   }
 
   void detect(const sensor_msgs::ImageConstPtr &msg) {}
